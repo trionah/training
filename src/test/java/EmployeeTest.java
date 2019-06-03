306c148693c294c7f53a8fce110fed7b552ff47e
Jeremy Smyth <jeremy@metablur.com>
2019-06-03 17:43:27 +0100

    add imports for assertions

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,6 +1,8 @@
 package test.java;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
