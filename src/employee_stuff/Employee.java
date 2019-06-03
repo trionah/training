846495f518b416d8fb3d1c16b8999fc5d65f74fd
Jeremy Smyth <jeremy@metablur.com>
2019-06-03 09:36:46 +0100

    add Employee class

---
+++ src/employee_stuff/Employee.java
@@ -0,0 +1,99 @@
+package employee_stuff;
+
+import java.util.Comparator;
+
+public class Employee {
+	private String name;
+	private int number;
+	private float salary;
+	private float bonus;
+	private static final float MIN_SALARY = 7000f;
+	private final float INITIAL_BONUS = 200f;
+	public static int instanceCount;
+
+	@Override
+	public void finalize() {
+		instanceCount--;
+	}
+
+	public Employee() {
+		//////////
+		instanceCount++;
+		this.bonus = INITIAL_BONUS;
+	}
+
+	public Employee(int newNumber) {
+		this();
+		this.setNumber(newNumber);
+	}
+
+	public Employee(int newNumber, String newName) {
+		this(newNumber);
+		// this.setNumber(newNumber);
+		this.setName(newName);
+	}
+
+	public Employee(int newNumber, String newName, float newSalary) {
+		this(newNumber, newName);
+//		this.setNumber(newNumber);
+//		this.setName(newName);
+		this.setSalary(newSalary);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+//	public boolean setNumber(String thisNumber) {
+//		return setNumber(Integer.parseInt(thisNumber));
+//	}
+
+	public boolean setNumber(int thisNumber) {
+		if (thisNumber > 0) {
+			this.number = thisNumber;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public boolean setSalary(float newSalary) {
+		if (newSalary >= MIN_SALARY) {
+			this.salary = newSalary;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public float calcPay() {
+		return getSalary() / 12;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Employee %d: %s, £%.2f. " + "Monthly gross pay: £%.2f.", this.getNumber(), this.getName(),
+				this.getSalary(), this.calcPay());
+	}
+
+	public static int surnameSort(Employee e1, Employee e2) {
+		if (!e1.getName().contains(" "))
+			return -1;
+		if (!e2.getName().contains(" "))
+			return 1;
+		return e1.getName().split(" ")[1].compareToIgnoreCase(e2.getName().split(" ")[1]);
+	}
+
+}
